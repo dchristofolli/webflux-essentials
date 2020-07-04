87fdf0bff26b604629d0140505504d03cdc7e113
dchristofolli <danielchristofolli@gmail.com>
2020-07-04 12:37:53 -0300

    initial commit

---
+++ src/main/java/com/dchristofolli/projects/webfluxessentials/WebfluxEssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.dchristofolli.projects.webfluxessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxEssentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxEssentialsApplication.class, args);
+    }
+
+}
