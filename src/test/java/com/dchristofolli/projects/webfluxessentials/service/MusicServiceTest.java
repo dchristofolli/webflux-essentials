9dbbd47705760a9c42fcb3c20dfaa79ab083cdaa
dchristofolli <danielchristofolli@gmail.com>
2020-07-04 20:34:05 -0300

    testes unitários

--- src/test/java/com/dchristofolli/projects/webfluxessentials/service/MusicServiceTest.java
+++ src/test/java/com/dchristofolli/projects/webfluxessentials/service/MusicServiceTest.java
@@ -2,16 +2,20 @@ package com.dchristofolli.projects.webfluxessentials.service;
 
 import com.dchristofolli.projects.webfluxessentials.domain.Music;
 import com.dchristofolli.projects.webfluxessentials.repository.MusicRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.blockhound.BlockHound;
 import reactor.blockhound.BlockingOperationError;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
@@ -43,5 +47,37 @@ class MusicServiceTest {
         } catch (Exception e) {
             Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
         }
+    }
+
+    @BeforeEach
+    public void setup(){
+        BDDMockito.when(musicRepository.findAll()).thenReturn(Flux.just(music));
+        BDDMockito.when(musicRepository.findById(ArgumentMatchers.anyInt())).thenReturn(Mono.just(music));
+    }
+
+    @Test
+    void findAll_ReturnFluxOfMusic_whenSuccessful(){
+        StepVerifier.create(musicService.findAll())
+                .expectSubscription()
+                .expectNext(music)
+                .verifyComplete();
+    }
+
+    @Test
+    void findById_ReturnMonoOfMusic_whenSuccessful(){
+        StepVerifier.create(musicService.findById(1))
+                .expectSubscription()
+                .expectNext(music)
+                .verifyComplete();
+    }
+
+    @Test
+    void findById_ReturnMonoError_whenEmptyMonoIsReturned(){
+        BDDMockito.when(musicRepository.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(Mono.empty());
+        StepVerifier.create(musicService.findById(1))
+                .expectSubscription()
+                .expectError(ResponseStatusException.class)
+                .verify();
     }
 }
